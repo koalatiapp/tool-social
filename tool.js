491c5d439de4da58ffd51c9a4320087f100c7975
ppalioune <ppalioune@gmail.com>
2021-01-29 08:55:16 -0500

    integration result-builder for tool-social

--- tool.js
+++ tool.js
@@ -1,37 +1,116 @@
 'use strict';
+const ResultBuilder = require('result-builder')
 
 class Tool {
     constructor({ page }) {
         this.page = page;
+        //Build result by result-builder
+        this.builder = new ResultBuilder()
     }
 
     async run() {
         await this._fetchMetas();
         this._validateMetas();
         this._generateTables();
+        this.checkFacebook()
+        this.checkTwitter()
     }
 
     get results() {
-        return [
-            {
-                'uniqueName': 'facebook',
-                'title': 'Facebook sharing optimization',
-                'description': 'Checks if your pages have all of the essential meta tags to look good when it is shared on Facebook.',
-                'weight': .5,
-                'score': this._scores.facebook,
-                'table': this._tables.facebook,
-                'recommendations': this._recommendations.facebook
-            },
-            {
-                'uniqueName': 'twitter',
-                'title': 'Twitter sharing optimization',
-                'description': 'Checks if your pages have all of the essential meta tags to look good when it is shared on Twitter.',
-                'weight': .5,
-                'score': this._scores.twitter,
-                'table': this._tables.twitter,
-                'recommendations': this._recommendations.twitter
-            },
-        ];
+        return this.builder.toArray()
+    }
+    checkFacebook() {
+        const SCORE_DEDUCTION_MAJOR = .5;
+        const SCORE_DEDUCTION_MINOR = .25;
+        const SCORE_DEDUCTION_CONSIDER = 0;
+
+        this._scores = {};
+        for (const domain of this._domains) {
+            this._scores[domain] = 1;
+        }
+
+        const result = this.builder.newTest("facebook")
+        result.setTitle("Facebook sharing optimization")
+            .setDescription("Checks if your pages have all of the essential meta tags to look good when it is shared on Facebook.")
+            .setWeight(.5)
+            .addTableRows(this._tables.facebook)
+        // Validate Facebook metas
+        if (!this._meta('og:title')) {
+            this._scores.facebook -= SCORE_DEDUCTION_MAJOR;
+            result.addRecommendation("Add an `og:title` meta tag to your page.");
+        } else if (this._meta('og:title').length > 55) {
+            this._scores.facebook -= SCORE_DEDUCTION_MINOR;
+            result.addRecommendation("Reduce the length of your `og:title` to 55 characters or under for better cross-platform visibility.");
+        }
+
+        if (!this._meta('og:description')) {
+            this._scores.facebook -= SCORE_DEDUCTION_MAJOR;
+            result.addRecommendation("Add a meta description to your page.");
+        } else if (this._meta('og:description').length > 200) {
+            this._scores.facebook -= SCORE_DEDUCTION_MINOR;
+            result.addRecommendation("Reduce the length of your `og:description` to 200 characters or under. A length of 55 characters or under is recommended for better cross-platform visibility.");
+        } else if (this._meta('og:description').length > 55) {
+            this._scores.facebook -= SCORE_DEDUCTION_CONSIDER;
+            result.addRecommendation("Consider reducing the length of your `og:description` to 55 characters or under for better cross-platform visibility.");
+        }
+
+        if (!this._meta('og:image')) {
+            this._scores.facebook -= SCORE_DEDUCTION_MAJOR;
+            result.addRecommendation("Add an `og:image` meta tag to your page. For more information, visit [Facebook's Guide to Sharing for Webmasters](https://developers.facebook.com/docs/sharing/webmasters/)");
+        }
+        result.setScore(this._scores.facebook)
+    }
+    checkTwitter() {
+        const SCORE_DEDUCTION_CRUCIAL = 1;
+        const SCORE_DEDUCTION_MAJOR = .5;
+        const SCORE_DEDUCTION_MINOR = .25;
+
+        this._scores = {};
+        for (const domain of this._domains) {
+            this._scores[domain] = 1;
+        }
+
+        const result = this.builder.newTest("twitter")
+        result.setTitle("Twitter sharing optimization")
+            .setDescription("Checks if your pages have all of the essential meta tags to look good when it is shared on Twitter.")
+            .setWeight(.5)
+            .addTableRows(this._tables.twitter)
+        // Validate Twitter metas
+        if (['summary', 'summary_large_image', 'app', 'player'].indexOf(this._meta('twitter:card')) == -1) {
+            this._scores.twitter -= SCORE_DEDUCTION_CRUCIAL;
+            result.addRecommendation("Add a valid `twitter:card` meta tag to your page. For more information, visit [Twitter's Getting Started with Cards Guide](https://developer.twitter.com/en/docs/tweets/optimize-with-cards/guides/getting-started)");
+        }
+
+        if (!this._meta('twitter:title')) {
+            this._scores.twitter -= SCORE_DEDUCTION_MAJOR;
+            result.addRecommendation("Add a `twitter:title` meta tag to your page.");
+        } else if (this._meta('twitter:title').length > 70) {
+            this._scores.twitter -= SCORE_DEDUCTION_MINOR;
+            result.addRecommendation("Reduce the length of your `twitter:title` to 70 characters or under.");
+        }
+
+        if (!this._meta('twitter:description')) {
+            this._scores.twitter -= SCORE_DEDUCTION_MAJOR;
+            result.addRecommendation("Add a meta description to your page.");
+        } else if (this._meta('twitter:description').length > 200) {
+            this._scores.twitter -= SCORE_DEDUCTION_MINOR;
+            result.addRecommendation("Reduce the length of your `twitter:description` to 200 characters or under.");
+        }
+
+        if (!this._meta('twitter:image')) {
+            this._scores.twitter -= SCORE_DEDUCTION_MAJOR;
+            result.addRecommendation("Add a `twitter:image` meta tag to your page. For more information, visit [Facebook's Guide to Sharing for Webmasters](https://developers.facebook.com/docs/sharing/webmasters/)");
+        }
+
+        if (this._meta('twitter:card') == 'player') {
+            const playerUrl = this._meta('twitter:player');
+
+            if (!playerUrl || !/^(?:https:\/\/|\/\/).*/.test(playerUrl)) {
+                this._scores.twitter -= SCORE_DEDUCTION_CRUCIAL;
+                result.addRecommendation("Add a valid `twitter:player` meta tag to your page. It is mandatory when using the player card. For more information, visit [Twitter's Player Card Documentation](https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/player-card)");
+            }
+        }
+        result.setScore(this._scores.twitter)
     }
 
     async cleanup() {
@@ -64,80 +143,7 @@ class Tool {
     };
 
     _validateMetas() {
-        const SCORE_DEDUCTION_CRUCIAL = 1;
-        const SCORE_DEDUCTION_MAJOR = .5;
-        const SCORE_DEDUCTION_MINOR = .25;
-        const SCORE_DEDUCTION_CONSIDER = 0;
 
-        this._recommendations = {};
-        this._scores = {};
-
-        for (const domain of this._domains) {
-            this._scores[domain] = 1;
-            this._recommendations[domain] = [];
-        }
-
-        // Validate Facebook metas
-        if (!this._meta('og:title')) {
-            this._scores.facebook -= SCORE_DEDUCTION_MAJOR;
-            this._recommendations.facebook.push("Add an `og:title` meta tag to your page.");
-        } else if (this._meta('og:title').length > 55) {
-            this._scores.facebook -= SCORE_DEDUCTION_MINOR;
-            this._recommendations.facebook.push("Reduce the length of your `og:title` to 55 characters or under for better cross-platform visibility.");
-        }
-
-        if (!this._meta('og:description')) {
-            this._scores.facebook -= SCORE_DEDUCTION_MAJOR;
-            this._recommendations.facebook.push("Add a meta description to your page.");
-        } else if (this._meta('og:description').length > 200) {
-            this._scores.facebook -= SCORE_DEDUCTION_MINOR;
-            this._recommendations.facebook.push("Reduce the length of your `og:description` to 200 characters or under. A length of 55 characters or under is recommended for better cross-platform visibility.");
-        } else if (this._meta('og:description').length > 55) {
-            this._scores.facebook -= SCORE_DEDUCTION_CONSIDER;
-            this._recommendations.facebook.push("Consider reducing the length of your `og:description` to 55 characters or under for better cross-platform visibility.");
-        }
-
-        if (!this._meta('og:image')) {
-            this._scores.facebook -= SCORE_DEDUCTION_MAJOR;
-            this._recommendations.facebook.push("Add an `og:image` meta tag to your page. For more information, visit [Facebook's Guide to Sharing for Webmasters](https://developers.facebook.com/docs/sharing/webmasters/)");
-        }
-
-
-        // Validate Twitter metas
-        if (['summary', 'summary_large_image', 'app', 'player'].indexOf(this._meta('twitter:card')) == -1) {
-            this._scores.twitter -= SCORE_DEDUCTION_CRUCIAL;
-            this._recommendations.twitter.push("Add a valid `twitter:card` meta tag to your page. For more information, visit [Twitter's Getting Started with Cards Guide](https://developer.twitter.com/en/docs/tweets/optimize-with-cards/guides/getting-started)");
-        }
-
-        if (!this._meta('twitter:title')) {
-            this._scores.twitter -= SCORE_DEDUCTION_MAJOR;
-            this._recommendations.twitter.push("Add a `twitter:title` meta tag to your page.");
-        } else if (this._meta('twitter:title').length > 70) {
-            this._scores.twitter -= SCORE_DEDUCTION_MINOR;
-            this._recommendations.twitter.push("Reduce the length of your `twitter:title` to 70 characters or under.");
-        }
-
-        if (!this._meta('twitter:description')) {
-            this._scores.twitter -= SCORE_DEDUCTION_MAJOR;
-            this._recommendations.twitter.push("Add a meta description to your page.");
-        } else if (this._meta('twitter:description').length > 200) {
-            this._scores.twitter -= SCORE_DEDUCTION_MINOR;
-            this._recommendations.twitter.push("Reduce the length of your `twitter:description` to 200 characters or under.");
-        }
-
-        if (!this._meta('twitter:image')) {
-            this._scores.twitter -= SCORE_DEDUCTION_MAJOR;
-            this._recommendations.twitter.push("Add a `twitter:image` meta tag to your page. For more information, visit [Facebook's Guide to Sharing for Webmasters](https://developers.facebook.com/docs/sharing/webmasters/)");
-        }
-
-        if (this._meta('twitter:card') == 'player') {
-            const playerUrl = this._meta('twitter:player');
-
-            if (!playerUrl || !/^(?:https:\/\/|\/\/).*/.test(playerUrl)) {
-                this._scores.twitter -= SCORE_DEDUCTION_CRUCIAL;
-                this._recommendations.twitter.push("Add a valid `twitter:player` meta tag to your page. It is mandatory when using the player card. For more information, visit [Twitter's Player Card Documentation](https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/player-card)");
-            }
-        }
     };
 
     async _fetchMetas() {
@@ -151,23 +157,21 @@ class Tool {
                 },
                 'facebook': {},
                 'twitter': {}
-    		};
+            };
 
             // Fetch Facebook's og:* metas
             for (const node of document.querySelectorAll('meta[property^="og:"]')) {
                 metas.facebook[node.getAttribute('property').toLowerCase()] = node.getAttribute('content');
             }
-
             // Fetch Twitter's twitter:* metas
             for (const node of document.querySelectorAll('meta[name^="twitter:"]')) {
                 metas.twitter[node.getAttribute('name').toLowerCase()] = node.getAttribute('content');
             }
-
             // Compute fallbacks
             function computeFallback(key, fallbackValue) {
                 const domain = key.indexOf('og:') === 0 ? 'facebook' : 'twitter';
                 if (typeof metas[domain][key] == 'undefined' || !metas[domain][key]) {
-                    metas[domain][key] = typeof fallbackValue == 'undefined' ?  null : fallbackValue;
+                    metas[domain][key] = typeof fallbackValue == 'undefined' ? null : fallbackValue;
                 }
             }
 
